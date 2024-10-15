4370b5cc59d488dfed1e74558b4a9bd8ef6e0336
Kostiantyn Kifor <ks.kifor@gmail.com>
2024-10-15 20:31:11 +0200

    test [DEV-1]: Implement app tests via spectator library

--- apps/frontend/frontsoulend/src/app/app.component.spec.ts
+++ apps/frontend/frontsoulend/src/app/app.component.spec.ts
@@ -1,30 +1,19 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 describe('AppComponent', () => {
-	beforeEach(async () => {
-		await TestBed.configureTestingModule({
-			imports: [AppComponent, NxWelcomeComponent, RouterModule.forRoot([])],
-		}).compileComponents();
+	let spectator: Spectator<AppComponent>;
+
+	const createComponent = createComponentFactory({
+		component: AppComponent,
 	});
 
-	it('should render title', () => {
-		const fixture = TestBed.createComponent(AppComponent);
-
-		fixture.detectChanges();
-
-		const compiled = fixture.nativeElement as HTMLElement;
-
-		expect(compiled.querySelector('h1')?.textContent).toContain('Welcome frontsoulend');
+	beforeEach(() => {
+		spectator = createComponent();
 	});
 
-	it(`should have as title 'frontsoulend'`, () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.componentInstance;
-
-		expect(app.title).toEqual('frontsoulend');
+	it('should create', () => {
+		expect(spectator.component).toBeTruthy();
 	});
 });
