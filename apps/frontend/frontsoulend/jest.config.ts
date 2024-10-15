486b7b8c0dc1e26da952c5eb7481d59575ba8c8c
Kostiantyn Kifor <ks.kifor@gmail.com>
2024-10-15 20:32:10 +0200

    fix [DEV-1]: Change paths in configuration files after changing folder structure

--- apps/frontend/frontsoulend/jest.config.ts
+++ apps/frontend/frontsoulend/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
 	displayName: 'frontsoulend',
-	preset: '../../jest.preset.js',
+	preset: '../../../jest.preset.js',
 	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-	coverageDirectory: '../../coverage/apps/frontsoulend',
+	coverageDirectory: '../../coverage/apps/frontend/frontsoulend',
 	transform: {
 		'^.+\\.(ts|mjs|js|html)$': [
 			'jest-preset-angular',
